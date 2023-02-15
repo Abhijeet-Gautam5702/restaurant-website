54ae197161f666384b603f30654b8eab60401ada
Abhijeet <abhijeetgautam572@gmail.com>
2023-02-15 23:04:25 +0530

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import "./Navbar.scss";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { motion } from "framer-motion";
+
 //variants
 const dropdownMenuAnimation = {
   open: {
-    transform: "translate(0,25%)",
+    display: "block",
+    opacity: [0, 1],
   },
   closed: {
-    transform: "translate(0,-100%)",
+    display: "none",
+    opacity: [1, 0],
   },
 };
 //variants
@@ -26,55 +29,60 @@ export default function Navbar() {
   return (
     <>
       <nav>
-        <div className="navbar__logo head-text">
-          <p>Restro</p>
+        {/* Navbar for large devices */}
+        <div className="navbar">
+          <div className="navbar__logo head-text">
+            <p>Restro</p>
+          </div>
+          <ul className="navbar__menu">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Menu</a>
+            </li>
+            <li>
+              <a href="">Order History</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+          </ul>
+
+          <motion.div
+            className="navbar__toggleMenu-icon"
+            onClick={() => setIsOpen((isOpen) => !isOpen)}
+            variants={toggleMenuAnimation}
+            animate={isOpen ? "open" : "closed"}
+            transition={{ duration: 0.3 }}
+          >
+            <GiHamburgerMenu />
+          </motion.div>
         </div>
-        <ul className="navbar__menu">
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">Menu</a>
-          </li>
-          <li>
-            <a href="">Order History</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-        </ul>
-
-        <motion.div
-          className="navbar__toggleMenu-icon"
-          onClick={() => setIsOpen((isOpen) => !isOpen)}
-          variants={toggleMenuAnimation}
-          animate={isOpen ? "open" : "closed"}
-          transition={{ duration: 0.3 }}
-        >
-          <GiHamburgerMenu />
-        </motion.div>
+        
+        {/* Navbar for small devices */}
+        {isOpen && (
+          <motion.ul
+            className="navbar__menu-dropdown"
+            animate={isOpen ? "open" : "closed"}
+            variants={dropdownMenuAnimation}
+            transition={{ duration: 0.3 }}
+          >
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Menu</a>
+            </li>
+            <li>
+              <a href="">Order History</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+          </motion.ul>
+        )}
       </nav>
-
-      <motion.ul
-        className="navbar__menu-dropdown"
-        animate={isOpen ? "open" : "closed"}
-        transition={{ duration: 0.3, type: "tween" }}
-        variants={dropdownMenuAnimation}
-        initial={true}
-      >
-        <li>
-          <a href="">Home</a>
-        </li>
-        <li>
-          <a href="">Menu</a>
-        </li>
-        <li>
-          <a href="">Order History</a>
-        </li>
-        <li>
-          <a href="">About</a>
-        </li>
-      </motion.ul>
     </>
   );
 }
